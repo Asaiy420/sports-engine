94ec6ffc0f1fb9c66e44f2c17257c73fde83ac5e
Asaiy420 <kcaryan05@gmail.com>
2026-03-04 00:17:24 +0545

    Add match router to main application for handling match-related routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
+import { matchRouter } from './routes/matches';
 
 const app = express();
 const PORT = process.env.PORT!;
 
 app.use(express.json());
+
+app.use('/matches', matchRouter);
 
 app.listen(PORT, () => {
   console.log(`The server is running on PORT: ${PORT}`);
