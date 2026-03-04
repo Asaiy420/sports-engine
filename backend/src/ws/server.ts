8b06b8613e164415ac5c4de6c81782cec45f0103
Asaiy420 <kcaryan05@gmail.com>
2026-03-04 21:32:03 +0545

    Implement Arcjet protection in WebSocket connection handling

--- backend/src/ws/server.ts
+++ backend/src/ws/server.ts
@@ -1,6 +1,8 @@
 import { WebSocket, WebSocketServer } from 'ws';
 import { Server as HttpServer } from 'http';
 import { matches } from '../db/schema';
+import { wsArcjet } from '../../arcjet';
+import type { Request } from 'express';
 
 interface ExtWebSocket extends WebSocket {
   isAlive: boolean;
@@ -26,7 +28,26 @@ export function attachWebSocketServer(server: HttpServer) {
     maxPayload: 1024 * 1024, // 1mb
   });
 
-  wss.on('connection', (socket: ExtWebSocket) => {
+  wss.on('connection', async (socket: ExtWebSocket, req: Request) => {
+    if (wsArcjet) {
+      try {
+        const decision = await wsArcjet.protect(req);
+
+        if (decision.isDenied()) {
+          const code = decision.reason.isRateLimit() ? 1013 : 1008;
+          const reason = decision.reason.isRateLimit()
+            ? 'Rate limit exceeded!'
+            : 'Access Denied';
+          socket.close(code, reason);
+          return;
+        }
+      } catch (e) {
+        console.error('Websocket Connection Error', e);
+        socket.close(1011, 'Server Security Error');
+        return;
+      }
+    }
+
     socket.isAlive = true;
     socket.on('pong', () => {
       socket.isAlive = true;
